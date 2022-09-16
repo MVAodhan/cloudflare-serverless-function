75a5cc90967acb677b0fb34ae8ed79829d316e77
MVAodhan <hamilton.aodhan@gmail.com>
2022-09-16 23:02:42 +1200

    testing

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,13 +1,15 @@
 export async function onRequestPost(context) {
-  try {
-    let input = await context.request.formData();
-    let pretty = JSON.stringify([...input], null, 2);
-    return new Response(pretty, {
+  const data = {
+    hello: 'world',
+  };
+
+  const json = JSON.stringify(data, null, 2);
+
+  return context.event.respondWith(
+    new Response(context, {
       headers: {
-        'Content-Type': 'application/json;charset=utf-8',
+        'content-type': 'application/json;charset=UTF-8',
       },
-    });
-  } catch (err) {
-    return new Response('Error parsing JSON content', { status: 400 });
-  }
+    })
+  );
 }
